1f467d64a282518928ab62a287aacbd4e2c7d0c6
Saad Moazzam <saad.mzm65@gmail.com>
2025-03-17 21:27:46 +0530

    Added client connection and disconnection feature

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,9 +1,10 @@
-use spacetimedb::{table, Identity};
+use spacetimedb::{reducer, table, Identity, ReducerContext, Table};
 
 #[table(name = client,public)]
 pub struct StdbClient {
     #[primary_key]
     client_id: Identity,
+    name: Option<String>,
     online: bool
 }
 
@@ -11,4 +12,29 @@ pub struct StdbClient {
 pub struct StdbVector2 {
     x: f32,
     y: f32
+}
+
+#[reducer]
+pub fn client_connected(ctx: &ReducerContext) {
+    if let Some(user) = ctx.db.client().client_id().find(ctx.sender) {
+        ctx.db.client().client_id().update(StdbClient {
+            online: true,
+            ..user
+        });
+    } else {
+        ctx.db.client().insert(StdbClient {
+            name: None,
+            client_id: ctx.sender,
+            online: true,
+        });
+    }
+}
+
+#[reducer]
+pub fn client_disconnected(ctx: &ReducerContext) {
+    if let Some(user) = ctx.db.client().client_id().find(ctx.sender) {
+        ctx.db.client().client_id().update(StdbClient { online: false, ..user });
+    } else {
+        log::warn!("Disconnect event for unknown client: {:?}", ctx.sender);
+    }
 }
